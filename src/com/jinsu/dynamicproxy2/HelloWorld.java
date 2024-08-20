cf3217799b52c577fce5697a61028848b4906326
Jinsu <jinsu8828@gmail.com>
2024-08-20 15:10:53 +0900

    add

---
+++ src/com/jinsu/dynamicproxy2/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.jinsu.dynamicproxy2;
+
+public interface HelloWorld {
+    
+    void sayHello();
+
+    void sayGoodbye();
+
+    void greet(String name);
+
+    void setLanguage(String language);
+
+    String getGreeting();
+}
